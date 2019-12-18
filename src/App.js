f79fbc9c22f3f9929365f77273b3c86d548fdc56
CodeCarrierlhd <mr_lhd1995@163.com>
2019-12-18 14:39:50 +0800

    init admin-sport

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * 应用根组件
+ */
+import React, { Component } from 'react'
+//BrowserRouter 路径不带# HashRouter路径带有#
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Admin from './pages/Admin/admin'
+export default class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>{/**有<Switch>标签，则其中的<Route>在路径相同的情况下，只匹配第一个，这个可以避免重复匹配；
+                 * 无<Switch>标签，则其中的<Route>在路径相同的情况下全都会匹配。更严重的是，还会匹配上级路径的 */}
+                    <Route path='/' component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
 }
-
-export default App;
